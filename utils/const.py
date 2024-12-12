4a59aefa8dfed3b97ff43cf55bfa1da55a680086
logtd <logoftd@gmail.com>
2024-12-12 09:03:07 -0700

    add const

---
+++ utils/const.py
@@ -0,0 +1,8 @@
+DEFAULT_SINGLE_LAYERS = {}
+for i in range(38):
+    DEFAULT_SINGLE_LAYERS[f'{i}'] = i > 19
+
+DEFAULT_DOUBLE_LAYERS = {}
+for i in range(19):
+    DEFAULT_DOUBLE_LAYERS[f'{i}'] = False
+
